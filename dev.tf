ac0d828f7339f316c830b713c2efcecae2e04d86
edyablo <eyablonsky@gmail.com>
2020-08-02 16:05:23 +0300

    split to dev and prod

--- dev.tf
+++ dev.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-
 resource "random_pet" "petname" {
   length    = 3
   separator = "-"
@@ -45,45 +44,4 @@ resource "aws_s3_bucket_object" "dev" {
   bucket       = aws_s3_bucket.dev.id
   content      = file("${path.module}/assets/index.html")
   content_type = "text/html"
-
-}
-
-resource "aws_s3_bucket" "prod" {
-  bucket = "${var.prod_prefix}-${random_pet.petname.id}"
-  acl    = "public-read"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${var.prod_prefix}-${random_pet.petname.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-
-  }
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_object" "prod" {
-  acl          = "public-read"
-  key          = "index.html"
-  bucket       = aws_s3_bucket.prod.id
-  content      = file("${path.module}/assets/index.html")
-  content_type = "text/html"
-
 }
